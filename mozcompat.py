d0900bd68fadd31ada39929f98a3f25fa88ff9cf
Hallvord R. M. Steen <hsteen@mozilla.com>
2013-11-05 21:43:18 +0100

    adding code to spoof JS environment. This is injected in on_resource_request_starting, but I'm not sure if that's the best place to have it - guess it will run too many times?

--- mozcompat.py
+++ mozcompat.py
@@ -21,6 +21,34 @@ if not os.path.exists('screenshots'):
 IOS_UA = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5'
 FOS_UA = 'Mozilla/5.0 (Mobile; rv:18.0) Gecko/18.0 Firefox/18.0'
 
+# Code for spoofing JS environment.
+# Note: perhaps we only need one of them - i.e. if the backend is webkit we may only need the other one?
+# TODO: this should be customized in a cleaner way, more tied to the UA string being used. We should have a range of UA strings and associated JS snippets to choose from
+IOS_SPOOF_SCRIPT = """(function(props){
+for(var name in props)
+navigator.__defineGetter__(name, (function(name){return function(){return props[name]}})(name));
+})({
+userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7',
+appCodeName: 'Mozilla',
+appName: 'Netscape',
+appVersion: '5.0 (iPhone; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7',
+vendor:'Apple Computer, Inc.',
+vendorSub:'',
+platform:'iPhone'
+});"""
+FOS_SPOOF_SCRIPT = """(function(props){
+for(var name in props)
+navigator.__defineGetter__(name, (function(name){return function(){return props[name]}})(name));
+})({
+userAgent: 'Mozilla/5.0 (Mobile; rv:18.1) Gecko/18.1 Firefox/18.1',
+appCodeName: 'Mozilla',
+appName: 'Netscape',
+appVersion: '5.0 (X11)',
+vendor:'',
+vendorSub:'',
+platform:'Linux i686'
+});"""
+
 SIMPLFY_SCRIPT = """
 function removeAttr(attr){
     var xpElms = document.evaluate('//*[@'+attr+']', document.documentElement, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
@@ -130,6 +158,10 @@ class Tab(WebKit.WebView):
     def _on_resource_request_starting(self, view, frame, resource,
                                       request, response):
         uri = request.get_uri()
+        if self._tab_type is 'fos':
+            self.execute_script(FOS_SPOOF_SCRIPT)
+        else:
+            self.execute_script(IOS_SPOOF_SCRIPT)
         if self._filter.match(uri):
             request.set_uri("about:blank")
             elements = self._find_element_all('[src="%s"]' % uri)
