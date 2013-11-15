8f15215143d8388d2a14132883c05eabe4a87e64
Seif Lotfy <seif@lotfy.com>
2013-11-15 14:44:37 +0100

    simplfy the rabbitMQ consumption

--- webcompat.py
+++ webcompat.py
@@ -8,37 +8,31 @@ import subprocess
 BASEPATH = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 BROWSER_CMD = "/usr/bin/python " + BASEPATH + "/browser.py %s"
 
+browsers = {}
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='mozcompat')
-queue = []
-browsers = {}
+channel.basic_qos(prefetch_count=1)
 
 
 def callback(channel, method, properties, body):
-    queue.append(body)
+    if not body in browsers:
+        browsers[body] = subprocess.Popen(BROWSER_CMD % (body), shell=True)
+    while not is_clean():
+        time.sleep(1)
+
+
+def is_clean():
+    values = [(key, value) for key, value in browsers.items()]
+    for key, value in values:
+        if value.poll() is not None:
+            del browsers[key]
+    return len(browsers) < 5
 
 
 if len(sys.argv) == 2 and sys.argv[1] == "listen":
-    while True:
-        time.sleep(1)
-        values = [(key, value) for key, value in browsers.items()]
-
-        for key, value in values:
-            if value.poll() is not None:
-                del browsers[key]
-
-        if not queue or len(browsers) > 5:
-            continue
-
-        uri = queue.pop(0)
-        if uri in browsers:
-            continue
-
-        channel.basic_consume(callback, queue='mozcompat', no_ack=True)
-        browser = subprocess.Popen(BROWSER_CMD % (uri), shell=True)
-        browsers[uri] = browser
-        print len(queue), len(browsers)
+    channel.basic_consume(callback, queue='mozcompat', no_ack=True)
+    channel.start_consuming()
 
 elif len(sys.argv) == 3 and sys.argv[1] == "push":
     channel.basic_publish(exchange='',
