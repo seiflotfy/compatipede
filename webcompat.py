29435c82800a07e15384023c5dac01f8efd169d7
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-06-13 15:41:06 +0200

    support running a country-specific list from arewecompatibleyet.com (rather than all lists)

--- webcompat.py
+++ webcompat.py
@@ -45,24 +45,37 @@ if len(sys.argv) == 2 and sys.argv[1] == "listen":
     channel.basic_consume(callback, queue='mozcompat', no_ack=True)
     channel.start_consuming()
 
-elif (len(sys.argv) == 2 or len(sys.argv) == 3) and sys.argv[1] == "rwcy":
+elif len(sys.argv) in [2,3,4] and (sys.argv[1] == "rwcy" or sys.argv[1] == "rwcylist"):
+    # support command lines like 
+    # webcompat.py rwcy
+    # webcompat.py rwcy 1050
+    # webcompat.py rwcylist jp50
+    # webcompat.py rwcylist jp50 35
+    if sys.argv[1] == "rwcylist" and len(sys.argv) == 2:
+            print('ERROR: list name missing')
+            quit()
     response = urllib2.urlopen(RWCY_URL)
     js_str = response.read()[len(RWCY_PREFIX):].strip()
     js = json.loads(js_str)
     # the "masterbugtable" JSON data structure: the hostIndex has one property for each host which has either open or closed bugs in bugzilla
     # In addition, the lists object contains local top lists - for example js["lists"]["no70"]["data"] is an array of (roughly) the top 70 sites in Norway
     # If we want to do a test run of *all* URLs in the system, including local lists, we should go through all these lists and add properties to masterbugtable's hostIndex
-    for key in js["lists"].keys():
-        for site in js["lists"][key]["data"]:
-            if not (site in js["hostIndex"]):
-                js["hostIndex"][site] = 1
+    if sys.argv[1] == "rwcy":
+            for key in js["lists"].keys():
+                for site in js["lists"][key]["data"]:
+                    if not (site in js["hostIndex"]):
+                        js["hostIndex"][site] = 1
     start_from = 0
-    if len(sys.argv) == 3:
+    if sys.argv[1] == "rwcy" and len(sys.argv) == 3:
         start_from = int(sys.argv[2])
         print 'rwcy lists from index %i' % start_from
+    elif sys.argv[1]  == "rwcylist" and len(sys.argv) == 4:
+        start_from = int(sys.argv[3])
+        print 'rwcy lists from index %i' % start_from
     counter = -1
-    print "%i sites to run through, starting from %i" % (len(js["hostIndex"].keys()), start_from)
-    for key in js["hostIndex"].keys():
+    the_list = js["hostIndex"].keys() if sys.argv[1] == "rwcy" else js['lists'][sys.argv[2]]['data']
+    print "%i sites to run through, starting from %i" % (len(the_list), start_from)
+    for key in the_list:
         counter += 1
         if key[-1] == "." or counter < start_from:
             continue
